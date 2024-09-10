daf106a4465f2cfe6d01aadd480c439530d79da9
Indrajit sahani <indrajit38mig@gmail.com>
2024-09-10 18:36:35 +0530

    added contact and logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home.jsx'
+import Home from './Pages/home.jsx'
 import Job from './Pages/job.jsx'
 import Join from './Pages/login.jsx'
 import About from './Pages/about.jsx'
-import { Contact } from 'lucide-react'
+import Contact from './Pages/contact.jsx'
 import Event from './Pages/event.jsx'
 
 
@@ -13,13 +13,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/job' element={<Job/>} />
-        <Route path='/event' element={<Event/>} />
-        <Route path='/join' element={<Join/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
+        <Route path='/job' element={<Job />} />
+        <Route path='/event' element={<Event />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<div>404 Page not found</div>} />
-        
+
       </Routes>
     </BrowserRouter>
 
