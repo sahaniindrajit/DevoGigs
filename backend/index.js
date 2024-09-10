bc5c42a736128002b7833d8284b7a3d2636924a1
Indrajit sahani <indrajit38mig@gmail.com>
2024-09-10 10:05:45 +0530

    project init

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import { mongoose } from 'mongoose';
+import router from "./routes/index.js"
+import 'dotenv/config';
+import morgan from 'morgan';
+import cors from 'cors'
+
+
+const port = process.env.PORT || 3000;
+const app = express()
+
+
+app.use(cors({
+    origin: 'https://localhost:5173',
+    credentials: true
+
+}));
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use('/api/v1', router)
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("DB connected")
+    }
+    catch (e) {
+        console.log(e)
+        process.exit(1);
+    }
+}
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server is running at port ${port}`);
+})
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        hiii! devogigs!
+      </div>
+    </>
+  )
+}
+
+export default App
